442120938ec54bdf37cbdc3faffd2753df0f6ad9
azoro3 <arthur.rozo@outlook.fr>
2016-12-19 15:26:53 +0100

    Revert "Update main.c"
    This reverts commit d375cf1df74d90835f9629f9dbe1a787e2604580.

--- main.c
+++ main.c
@@ -11,13 +11,29 @@
  * Created on 15 décembre 2016, 15:54
  */
 
-+#include <stdio.h>
-+#include <stdlib.h>
-+
-+/*
-+ * 
-+ */
-+int main(int argc, char** argv) {
-+
-+    return (EXIT_SUCCESS);
-+}
+/ Pour les constantes EXIT_SUCCESS et EXIT_FAILURE /
+#include <stdlib.h>
+/ Pour fprintf() /
+#include <stdio.h>
+/ Pour fork() /
+#include <unistd.h>
+/ Pour perror() et errno /
+#include <errno.h>
+/ Pour le type pid_t /
+#include <sys/prctl.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#define CLEF 123456 
+#include <sys/shm.h>
+#define PERMS 777
+int docs;
+struct document
+{
+char titre[20];
+char text[1000];
+char proprio[8];
+char connecte[10][8];
+};
+typedef struct
