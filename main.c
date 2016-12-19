96f1c311bc7f5108b252954720b662d1d8f02fd4
Unknown <arthur.rozo@outlook.fr>
2016-12-19 18:02:43 +0100

    Ecriture
    Ecriture dans un fichier de la saisie en ligne de commande par deux processus

--- main.c
+++ main.c
@@ -1,39 +1,93 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: alexandre
- *
- * Created on 15 décembre 2016, 15:54
- */
-
-/ Pour les constantes EXIT_SUCCESS et EXIT_FAILURE /
-#include <stdlib.h>
-/ Pour fprintf() /
 #include <stdio.h>
-/ Pour fork() /
+/* Pour les constantes EXIT_SUCCESS et EXIT_FAILURE */
+#include <stdlib.h>
+/* Pour fprintf() */
+#include <stdio.h>
+/* Pour fork() */
 #include <unistd.h>
-/ Pour perror() et errno /
+/* Pour perror() et errno */
 #include <errno.h>
-/ Pour le type pid_t /
-#include <sys/prctl.h>
-#include <sys/wait.h>
-#include <string.h>
+/* Pour le type pid_t */
 #include <sys/types.h>
-#include <sys/ipc.h>
-#define CLEF 123456 
-#include <sys/shm.h>
-#define PERMS 777
-int docs;
+#include <sys/prctl.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/*
+ *
+ */
+int main(int argc, char** argv) {
 struct document
 {
-char titre[20];
-char text[1000];
-char proprio[8];
-char connecte[10][8];
+    char titre[20];
+    char *text;
+    char proprio[8];
+    char connecte[10][8];
 };
-typedef struct
+
+int main(int argc, char *argv[])
+{
+    struct document mesDoc[5];
+    /*test écriture*/
+    FILE* fichier = NULL;
+    fichier = fopen("./test.txt", "a+");
+    if (fichier != NULL)
+    {
+        printf ("fichier existant \n");
+        fclose(fichier);
+    }
+
+    return (EXIT_SUCCESS);
+}
+
+
+    int argv0size = strlen(argv[0]);
+    int pid1 = fork();
+
+    if(pid1<0)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if(pid1==0)
+    {
+        FILE* fichier = NULL;
+        fichier = fopen("./test.txt", "a+");
+        printf("je vais écrire");
+        strncpy(argv[0],"un",argv0size);
+        char saisie1[30]="";
+        fgets(saisie1, sizeof(saisie1), stdin);
+        fprintf(fichier,saisie1);
+        fclose(fichier);
+        sleep(1000);
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        int pid2 = fork();
+        if(pid2<0)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else if(pid2==0)
+        {
+            FILE* fichier = NULL;
+            fichier = fopen("./test.txt", "a+");
+            strncpy(argv[0],"deux",argv0size);
+            char saisie2[30]="";
+            fgets(saisie2, sizeof(saisie2), stdin);
+            fprintf(fichier,saisie2);
+            fclose(fichier);
+            sleep(1000);
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            int status;
+            int rpid2 = waitpid(pid2,&status,0);
+            int rpid = waitpid(pid1,&status,0);
+        }
+    }
+    return EXIT_SUCCESS;
+} 
