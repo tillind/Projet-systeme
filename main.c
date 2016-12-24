758a07c7f8eedbabc16b093a1b301353c652bf1a
Unknown <arthur.rozo@outlook.fr>
2016-12-24 15:31:08 +0100

    Fonctions
    Fonctions écriture OK

--- main.c
+++ main.c
@@ -24,31 +24,30 @@ int main(int argc, char** argv) {
         char proprio[8];
         char connecte[10][8];
     };
-    void write (FILE* fichier,struct document mastruct)/*w,r,a+,...*/
+    void write (FILE* fichier,char *chemin,struct document mastruct)/*w,r,a+,...*/
         {   
-            fichier = fopen("./test.txt", "r+");
-            if (fichier != NULL)
-            {
-                printf ("fichier existant \n");
-                fputs(mastruct.text,fichier);
-                fclose(fichier);
-            }
+            fichier = fopen(chemin, "a+");
+            fputs(mastruct.text,fichier);
+            fputs("\n",fichier);
+            fclose(fichier);
         }
-        void read (FILE* fichier, struct document str)
+        void read (FILE* fichier, char *chemin, struct document str)
         {
-            
-        };
+            #define TAILLE_MAX 1000 // Tableau de taille 1000
+            char chaine[TAILLE_MAX] = ""; // Chaîne vide de taille TAILLE_MAX
+            fichier = fopen(chemin, "r");
+            while (fgets(chaine, TAILLE_MAX, fichier) != NULL) // On lit le fichier tant qu'on ne reçoit pas d'erreur (NULL)
+            {
+                strcat(str.text,chaine); // On insère ce qu'on vient de lire
+            }
+            fclose(fichier);
+        }
     struct document mesDoc[5];
     /*test écriture*/
-    mesDoc[5].text="test";
+    mesDoc[5].text="bonjour tout le monde \n";
     FILE* fichier = NULL;
-    fichier = fopen("./test.txt", "a+");
-    if (fichier != NULL)
-    {
-        printf ("fichier existant \n");
-        write(fichier,mesDoc[5]);
-        fclose(fichier);
-    }
-
+    write(fichier,"./test.txt",mesDoc[5]);
+    read(fichier,"./test.txt",mesDoc[5]);
+    printf("%s",mesDoc[5].text);
     return (EXIT_SUCCESS);
 }
